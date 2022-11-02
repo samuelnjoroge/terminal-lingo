a6849209d844af6d9597867eefd456930f3f0925
Samuel Mbuga Njoroge <samuelmbuganjoroge@thehiddroject34.attlocal.net>
2022-11-01 17:03:37 -0700

    first commit

---
+++ terminal_lingo.py
@@ -0,0 +1,21 @@
+def terminal_lingo():
+   command = input("Type a command for a definition: ")
+   if (command == "touch"):
+      print("\n")
+      print("This command allows you to create a file")
+   if (command == "mkdir"):
+      print("\n")
+      print("This command creates new folder")
+   if (command == "cd"):
+      print("\n")
+      print("This command allows you to move between folders")
+   if (command == "ls"):
+      print("\n")
+      print("This commmand allows you to list all flies in a folder")
+   if (command == "cat"):
+      print ("\n")
+      print ("this command reads data from the file and gives content as a ouput")
+
+
+
+terminal_lingo()
